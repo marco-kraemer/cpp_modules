757d83155856f04d1d8dee0fefe82200d97dbe72
marco-kraemer <marco.k.santos@example.com>
2021-07-16 08:24:39 -0300

    update

--- ex00/megaphone/megaphone.cpp
+++ ex00/megaphone/megaphone.cpp
@@ -6,14 +6,39 @@
 /*   By: maraurel <maraurel@student.42sp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/16 07:54:48 by maraurel          #+#    #+#             */
-/*   Updated: 2021/07/16 07:54:50 by maraurel         ###   ########.fr       */
+/*   Updated: 2021/07/16 08:20:31 by maraurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	std::cout << "Hello World!\n";
+	int	i;
+	int	j;
+	int	temp;
+
+	if (argc == 1)
+	{
+		std::cout << "LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (1);
+	}
+	i = 1;
+	while(argv[i])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (argv[i][j] <= 'z' && argv[i][j] >= 'a')
+				temp = (int)argv[i][j] - 32;
+			else
+				temp = (int)argv[i][j];
+			std::cout << (char)temp;
+			j++;
+		}
+		std::cout << " ";
+		i++;
+	}
+	std::cout << "\n";
 	return  (0);
 }
