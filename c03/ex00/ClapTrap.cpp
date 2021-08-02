40c11aa1b8542cd6c4b94bb48df4c57693758e10
marco-kraemer <marco.k.santos@example.com>
2021-08-02 09:11:14 -0300

    update

--- c03/ex00/ClapTrap.cpp
+++ c03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: maraurel <maraurel@student.42sp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 08:31:26 by maraurel          #+#    #+#             */
-/*   Updated: 2021/08/02 08:48:10 by maraurel         ###   ########.fr       */
+/*   Updated: 2021/08/02 09:10:32 by maraurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 ClapTrap::ClapTrap()
 {
 	std::cout << "Default constructor called" << std::endl;
+	Name = "";
 	Hitpoints = 10;
 	EnergyPoints = 10;
 	AttackDamage = 10;
@@ -41,4 +42,29 @@ ClapTrap& ClapTrap::operator=(const ClapTrap &clap)
 	this->Hitpoints = clap.Hitpoints;
 	this->EnergyPoints = clap.EnergyPoints;
 	this->AttackDamage = clap.AttackDamage;
+	return (*this);
+}
+
+ClapTrap::ClapTrap(const ClapTrap &clap)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = clap;
+}
+
+void	ClapTrap::attack(std::string const & target)
+{
+	std::cout << "ClapTrap " << this->Name << " attacks " << target << ", causing "
+		<< this->AttackDamage << " points of damage!" << std::endl;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	std::cout << "ClapTrap " << this->Name << " took " << amount << " of damage!" << std::endl;
+	this->Hitpoints -= amount;
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	std::cout << "ClapTrap " << this->Name << " received " << amount << " hit points!" << std::endl;
+	this->Hitpoints += amount;
 }
--- c03/ex00/main.cpp
+++ c03/ex00/main.cpp
@@ -6,7 +6,21 @@
 /*   By: maraurel <maraurel@student.42sp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 08:31:59 by maraurel          #+#    #+#             */
-/*   Updated: 2021/08/02 08:32:00 by maraurel         ###   ########.fr       */
+/*   Updated: 2021/08/02 09:08:29 by maraurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+int	main(void)
+{
+	ClapTrap	user1("user1");
+	ClapTrap	user2("user2");
+
+	user1.attack("user3");
+	user1.takeDamage(5);
+	user1.beRepaired(10);
+	user2.attack("user4");
+	user2.takeDamage(3);
+	user2.beRepaired(25); 
+}
