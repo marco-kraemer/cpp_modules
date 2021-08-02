c1c3349666c82406e7db6faf9f9c5e4ad22d8718
marco-kraemer <marco.k.santos@example.com>
2021-08-02 11:27:14 -0300

    update

--- c03/ex01/ScavTrap.hpp
+++ c03/ex01/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: maraurel <maraurel@student.42sp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 09:21:17 by maraurel          #+#    #+#             */
-/*   Updated: 2021/08/02 10:08:05 by maraurel         ###   ########.fr       */
+/*   Updated: 2021/08/02 11:26:52 by maraurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ class ScavTrap : public ClapTrap
 		virtual void		attack(std::string const & target);
 		virtual void		takeDamage(unsigned int amount);
 		virtual void		beRepaired(unsigned int amount);
-		virtual void		guardGate();
+		void			guardGate();
 
 };
 
