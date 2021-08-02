a5fd0dc25899055efd9d7c2a42115b7ef484216a
marco-kraemer <marco.k.santos@example.com>
2021-08-02 09:13:39 -0300

    update

--- c03/ex00/main.cpp
+++ c03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: maraurel <maraurel@student.42sp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 08:31:59 by maraurel          #+#    #+#             */
-/*   Updated: 2021/08/02 09:08:29 by maraurel         ###   ########.fr       */
+/*   Updated: 2021/08/02 09:12:59 by maraurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int	main(void)
 	ClapTrap	user1("user1");
 	ClapTrap	user2("user2");
 
-	user1.attack("user3");
-	user1.takeDamage(5);
-	user1.beRepaired(10);
-	user2.attack("user4");
-	user2.takeDamage(3);
-	user2.beRepaired(25); 
+	user1.attack("user2");
+	user2.takeDamage(10);
+	user2.beRepaired(2);
+	user2.attack("user1");
+	user1.takeDamage(10);
+	user1.beRepaired(1);
 }
