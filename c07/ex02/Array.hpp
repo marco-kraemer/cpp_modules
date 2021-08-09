ea30dc7f9f5bcda631a4485f20fed6affaa2b3aa
marco-kraemer <marco.k.santos@example.com>
2021-08-09 12:04:21 -0300

    update

--- c07/ex02/Array.hpp
+++ c07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: maraurel <maraurel@student.42sp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 10:37:07 by maraurel          #+#    #+#             */
-/*   Updated: 2021/08/09 11:34:51 by maraurel         ###   ########.fr       */
+/*   Updated: 2021/08/09 12:04:09 by maraurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,30 +18,35 @@ template <typename T>
 class Array
 {
 	private:
-		T array[];
+		T	*array;
+		int	length;
 	public:
 		Array();
 		Array(unsigned int n);
 		~Array();
 		Array(const Array &p);
 		Array& operator=(const Array &p);
+		T& operator[](unsigned int i);
 };
 
 template <typename T>
 Array<T>::Array()
 {
-	array = new T;
+	array(nullptr);
+	length = 0;
 }
 
 template <typename T>
-Array<T>::Array(unsigned int n)
+Array<T>::Array(unsigned int n) : array(nullptr)
 {
-	array[n] = *new T;
+	length = 0;
+	array = new T[n];
 }
 
 template <typename T>
 Array<T>::~Array()
 {
+	delete[] this->array;
 }
 
 template <typename T>
@@ -50,6 +55,18 @@ Array<T>::Array(const Array &p)
 	*this = p;
 }
 
+template <typename T>
+Array<T>&	Array<T>::operator=(const Array &p)
+{
+	for (int i = 0; p.array[i]; i++)
+		this->array[i] = p.array[i];
+	return (*this);
+}
 
+template <typename T>
+T&	Array<T>::operator[](unsigned int i)
+{
+	return (this->array[i]);
+}
 
 #endif
